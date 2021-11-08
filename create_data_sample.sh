d89b7120a0ccd418212776b2395fc8335be35e87
Roman Hillje <roman.hillje@googlemail.com>
2021-11-08 16:15:29 +0100

    Format time stamp in log messages

--- create_data_sample.sh
+++ create_data_sample.sh
@@ -8,6 +8,10 @@ if [ $# -lt 4 ]; then
     exit 1
 fi
 
+time_stamp() {
+    echo $(date +"%Y-%m-%d %T")
+}
+
 INPUT_DIR=${1}
 OUTPUT_DIR=${2}
 GENES_RE="\\(${3//,/\\|}\\)"
@@ -15,44 +19,44 @@ GENES_=${3//,/_}
 STUDIES_RE="\\(${4//,/\\|}\\)"
 STUDIES_=${4//,/_}
 
-echo "$(date) Filtering genes index (1/11)"
+echo "[$(time_stamp)] Filtering genes index (1/11)"
 mkdir -p ${OUTPUT_DIR}/lut/genes-index/
 grep -h "\"gene_id\":\"${GENES_RE}\"" ${INPUT_DIR}/lut/genes-index/part-*.json > ${OUTPUT_DIR}/lut/genes-index/part-${GENES_}.json
 
-echo "$(date) Filtering variant index (2/11)"
+echo "[$(time_stamp)] Filtering variant index (2/11)"
 mkdir -p ${OUTPUT_DIR}/lut/variant-index/
 grep -h "\"gene_id_any\":\"${GENES_RE}\"" ${INPUT_DIR}/lut/variant-index/part-*.json > ${OUTPUT_DIR}/lut/variant-index/part-${GENES_}.json
 
-echo "$(date) Filtering study index (3/11)"
+echo "[$(time_stamp)] Filtering study index (3/11)"
 mkdir -p ${OUTPUT_DIR}/lut/study-index/
 grep -h "\"study_id\":\"${STUDIES_RE}\"" ${INPUT_DIR}/lut/study-index/part-*.json > ${OUTPUT_DIR}/lut/study-index/part-${STUDIES_}.json
 
-echo "$(date) Filtering study overlap index (4/11)"
+echo "[$(time_stamp)] Filtering study overlap index (4/11)"
 mkdir -p ${OUTPUT_DIR}/lut/overlap-index/
 grep -h "\"A_study_id\":\"${STUDIES_RE}\"" ${INPUT_DIR}/lut/overlap-index/part-*.json > ${OUTPUT_DIR}/lut/overlap-index/part-${STUDIES_}.json
 
-echo "$(date) Filtering variant to gene records (5/11)"
+echo "[$(time_stamp)] Filtering variant to gene records (5/11)"
 mkdir -p ${OUTPUT_DIR}/v2g/
 grep -h "\"gene_id\":\"${GENES_RE}\"" ${INPUT_DIR}/v2g/part-*.json > ${OUTPUT_DIR}/v2g/part-${GENES_}.json
 
-echo "$(date) Filtering variant to disease records (6/11)"
+echo "[$(time_stamp)] Filtering variant to disease records (6/11)"
 mkdir -p ${OUTPUT_DIR}/v2d/
 grep -h "\"study_id\":\"${STUDIES_RE}\"" ${INPUT_DIR}/v2d/part-*.json > ${OUTPUT_DIR}/v2d/part-${STUDIES_}.json
 
-echo "$(date) Filtering disease to variant to gene records (7/11)"
+echo "[$(time_stamp)] Filtering disease to variant to gene records (7/11)"
 mkdir -p ${OUTPUT_DIR}/d2v2g/
 grep -h "\"study_id\":\"${STUDIES_RE}\"" ${INPUT_DIR}/d2v2g/part-*.json | grep "\"gene_id\":\"${GENES_RE}\"" > ${OUTPUT_DIR}/d2v2g/part-${STUDIES_}-${GENES_}.json
 
-
-echo "$(date) Filtering variant to disease colocation records (8/11)"
+echo "[$(time_stamp)] Filtering variant to disease colocation records (8/11)"
 mkdir -p ${OUTPUT_DIR}/v2d_coloc/
 grep -h ":\"${STUDIES_RE}\"" ${INPUT_DIR}/v2d_coloc/part-*.json | grep ":\"${GENES_RE}\"" > ${OUTPUT_DIR}/v2d_coloc/part-${STUDIES_}-${GENES_}.json
 
-echo "$(date) Filtering variant to disease credset records (9/11)"
+echo "[$(time_stamp)] Filtering variant to disease credset records (9/11)"
 mkdir -p ${OUTPUT_DIR}/v2d_credset/
 zcat ${INPUT_DIR}/v2d_credset/part-*.json.gz | grep -h "\"study_id\":\"${STUDIES_RE}\"" | gzip > ${OUTPUT_DIR}/v2d_credset/part-${STUDIES_}.json.gz
 
-echo "$(date) Filtering gwas records (10/11)"
+echo "[$(time_stamp)] Filtering gwas records (10/11)"
+
 mkdir -p ${OUTPUT_DIR}/sa/
 IFS=',' read -r -a genes <<< "$3"
 IFS=',' read -r -a studies <<< "$4"
@@ -60,10 +64,11 @@ for f in ${INPUT_DIR}/sa/gwas/*.parquet; do
     ./parquet_rows_filter_cli/parquet_filter.py "${f}" ${OUTPUT_DIR}/sa/gwas/"$(basename -- $f)" ${studies[@]/#/--study_id=}
     echo .
 done
-echo "$(date) Filtering molecular trait records (11/11)"
+
+echo "[$(time_stamp)] Filtering molecular trait records (11/11)"
 for f in ${INPUT_DIR}/sa/molecular_trait/*.parquet; do
     ./parquet_rows_filter_cli/parquet_filter.py "${f}" ${OUTPUT_DIR}/sa/molecular_trait/"$(basename -- $f)" "${studies[@]/#/--study_id=}" "${genes[@]/#/--gene_id=}"
     echo .
 done
 
-echo "$(date) Creating of the sample dataset has finished."
+echo "[$(time_stamp)] Creating of the sample dataset has finished."
